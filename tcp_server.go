821be46cb1fc9124bc107053ad626a02d8804829
Chris <christiaea@uia.no>
2023-04-21 10:05:14 +0200

    alfsem03

--- tcp_server.go
+++ tcp_server.go
@@ -37,7 +37,7 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.AlfSem03, len(mycrypt.AlfSem03)-4)
 log.Println("Dekrypter melding: ", string(dekryptertMelding))
 switch msg := string(dekrypterMelding) {
   				        case "ping":
